bf65a501065dda3a57d997c1a9d5f443751b3c77
nghia nguyen <nghiank@hotmail.com>
2015-11-29 17:22:33 -0800

    export some important

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 /**
  * Created by nghia on 11/26/15.
  */
+
+module.exports.FunctionSignature = require('./src/functionSignature');
+module.exports.Type = require('./src/type');
+module.exports.CodeGenerateFactory = require('./src/codeGenerateFactory');
+module.exports.allTypes = require('./src/allTypes');
+
+
+
